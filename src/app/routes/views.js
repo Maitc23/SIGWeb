26515b27cda14de97b6ee17df97535dfe5d138ee
pris14gomez <gabriela.penagos14@gmail.com>
2019-10-03 00:44:45 -0500

    landing y construccion

--- src/app/routes/views.js
+++ src/app/routes/views.js
@@ -11,43 +11,40 @@ const router = require('express').Router();
  *      BORRALO SUBRNOMAL
  */
 
-router.get('/', function (req, res) {
+router.get('/index.ejs', function(req, res) {
     res.render('index', {
-      message: req.flash('loginMessage')
-    });
-});
-
-
-router.get('/login.ejs', function (req, res) {
-    res.render('login',
-      {
         message: req.flash('loginMessage')
-      });
-});
-
-
-router.get('/header.ejs', function (req, res) {
-  res.render('header',
-    {
-      message: req.flash('loginMessage')
     });
 });
 
 
-router.get('/header.ejs', function (req, res) {
-  res.render('header',
-    {
-      message: req.flash('loginMessage')
+router.get('/login.ejs', function(req, res) {
+    res.render('login', {
+        message: req.flash('loginMessage')
+    });
+});
+
+
+router.get('/header.ejs', function(req, res) {
+    res.render('header', {
+        message: req.flash('loginMessage')
+    });
+});
+
+
+router.get('/header.ejs', function(req, res) {
+    res.render('header', {
+        message: req.flash('loginMessage')
     });
 });
 
 /**
  * --Temporaly public view
  */
-router.get('/inventory.ejs', function (req, res) {
-  res.render('inventory', {
-    //message: req.flash('loginMessage')
-  });
+router.get('/inventory.ejs', function(req, res) {
+    res.render('inventory', {
+        //message: req.flash('loginMessage')
+    });
 });
 
 
@@ -55,14 +52,27 @@ router.get('/inventory.ejs', function (req, res) {
  * --- Private views ---
  */
 
-router.get('/perfil.ejs', function (req, res) {
+router.get('/perfil.ejs', function(req, res) {
     res.render('perfil', {
-      message: req.flash('ConnectionMessage')
-    }); 
+        message: req.flash('ConnectionMessage')
+    });
 });
 
-router.get('/logout', function (req, res) {
+router.get('/logout', function(req, res) {
     req.logout();
     req.redirect();
-  });
+});
 module.exports = router;
+
+/** Gabriela Agrego esto porque lo necesita */
+router.get('/landing.ejs', function(req, res) {
+    res.render('landing', {
+        /* message: req.flash('loginMessage')*/
+    });
+});
+/** Gabriela Agrego esto porque lo necesita */
+router.get('/construccion.ejs', function(req, res) {
+    res.render('construccion', {
+        /* message: req.flash('loginMessage')*/
+    });
+});
