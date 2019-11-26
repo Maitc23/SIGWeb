f2a9fafff248ab7e828ee62abd61d692b4bd1ce0
MitPatel\mit <mitpatel2599@gmail.com>
2019-11-25 23:53:42 -0500

    Cerrar sesion(arreglar algunos detalles)

---
+++ src/public/js/nuevapieza.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+    $.ajax({
+        type: 'get',
+        url: '/nuevapieza',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        headers: {
+            'Authorization': 'bearer ' + window.localStorage.getItem('token')
+        },
+        success: function (data) {
+                
+        },
+        error: function (xhr, status, error) {
+            if (xhr && xhr.responseJSON && xhr.responseJSON.error) {
+                //alert(xhr.responseJSON.error);
+                //alert(error);
+                alert("Debe iniciar sesión primero.");
+                window.location.href = '/';
+            }
+        }
+
+    });
+}
